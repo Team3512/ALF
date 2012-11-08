7327235bcc84247cbe546fd4c293babe4b9507b8
Tyler Veness <calcmogul@gmail.com>
2012-11-07 23:48:29 -0800

    Made program call taskSpawn to restart the robot code

--- Main.cpp
+++ Main.cpp
@@ -5,15 +5,30 @@
 //Author: FRC Team 3512, Spartatroniks
 //=============================================================================
 
+#include <stdio.h>
+#include <ioLib.h>
 #include <loadLib.h>
 #include <unldLib.h>
+#include <taskLib.h>
 #include <fcntl.h>
 
-int main() {
-    unldByNameAndPath( "FRC_UserProgram.out" , "ni-rt/system" , UNLD_CPLUS_XTOR_AUTO );
+INT32 FRC_UserProgram_StartupLibraryInit();
 
-    int program = open( "ni-rt/system/FRC_UserProgram.out" , O_RDONLY , 0 );
+int ALFmain() {
+    printf( "ALF.out started\n" );
+
+    unldByNameAndPath( "FRC_UserProgram.out" , "/ni-rt/system" , UNLD_CPLUS_XTOR_AUTO );
+
+    int program = open( "/ni-rt/system/FRC_UserProgram.out" , O_RDONLY , 0 );
     loadModule( program , LOAD_ALL_SYMBOLS | LOAD_CPLUS_XTOR_AUTO );
+    close( program );
+
+    taskSpawn( "FRC_RobotTask" , // Task name
+            101 , // Priority
+            VX_FP_TASK , // Task option flag
+            0xFFFF , // Stack size
+            (FUNCPTR)FRC_UserProgram_StartupLibraryInit , // Entry point
+            0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ); // unused task args
 
     return 0;
 }
